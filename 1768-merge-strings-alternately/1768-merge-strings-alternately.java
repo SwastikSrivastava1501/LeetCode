6a71a04bdf0d1f08b69db8572dec421bf79e95d9
Swastik Srivastava <sarvidhi777@gmail.com>
2026-08-04 09:46:08 +0530

    Time: 1 ms (96.08%), Space: 43.1 MB (63.64%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -4,15 +4,14 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         StringBuilder result = new StringBuilder();
+        int i=0;
 
-        int i=0, j=0;
-
-        while(i < m || j < n) {
+        for(i = 0; i < Math.max(m,n); i++) {
             if(i < m) {
-                result.append(word1.charAt(i++));
+                result.append(word1.charAt(i));
             }
-            if(j < n) {
-                result.append(word2.charAt(j++));
+            if(i < n) {
+                result.append(word2.charAt(i));
             }
         }
         return result.toString();
