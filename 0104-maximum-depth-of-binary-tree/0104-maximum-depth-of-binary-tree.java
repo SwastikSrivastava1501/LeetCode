d670d6f5ad55cb786652dbdc4ad62f0f4ffcf379
Swastik Srivastava <sarvidhi777@gmail.com>
2026-08-31 10:13:43 +0530

    Time: 0 ms (100%), Space: 46.8 MB (91.35%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,10 +18,10 @@ class Solution {
         if(root == null){
              return 0;
         }
+
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
 
         return 1 + Math.max(leftDepth, rightDepth);
-
     }
 }
